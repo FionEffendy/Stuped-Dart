9c6a0af5e7997572cf600e61042d9935859ba7c2
F10N <fion.foef@gmail.com>
2023-12-11 13:28:30 +0700

    merubah file oprator

--- Pengenalan/bin/oprator.dart
+++ Pengenalan/bin/oprator.dart
@@ -1,8 +1,22 @@
 void main() {
-  print('Selamat Siang');
-  name('Dizz', 'Nutt');
-}
+  var a = 10;
+  var b = 5;
 
-void name(String namadep, String namablk) {
-  print(namadep + namablk);
+  print('Oprator Aritmatika');
+  print('hasil penjumlahan : ${a + b}');
+  print('hasil pengurangan : ${a - b}');
+  print('hasil perkalian : ${a * b}');
+  print('hasil pembagian (double) : ${a / b}');
+  print('hasil pembagian (int) : ${a ~/ b}');
+  print('hasil sisabagi : ${a % b}');
+  print('');
+
+  print('Oprator Perbandingan');
+  print('oprator sama dengan : ${a == b}');
+  print('oprator tidak sama dengan : ${a != b}');
+  print('oprator lebih dari : ${a > b}');
+  print('oprator kurang dari : ${a < b}');
+  print('oprator lebih dari sama dengan : ${a >= b}');
+  print('oprator kurang dari sama dengan : ${a <= b}');
+  print('');
 }
